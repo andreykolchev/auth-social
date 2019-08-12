e059f51eec5aadb974ddc32c72c76d68da9c389c
andreykolchev <andreykolchev@gmail.com>
2019-08-12 16:05:42 +0300

    add profileId to token claims

--- src/main/kotlin/com/pirates/auth/service/TokenService.kt
+++ src/main/kotlin/com/pirates/auth/service/TokenService.kt
@@ -17,7 +17,7 @@ class TokenService(private val jwtProperties: JWTProperties,
 
     fun getTokenByUserCredentials(userEntity: UserEntity): String {
         return jwtService.genToken(
-                claims = mapOf<String, Any>(PERSON_ID_CLAIM to userEntity.personId),
+                claims = mapOf<String, Any>(PERSON_ID_CLAIM to userEntity.personId, PROFILE_ID_CLAIM to userEntity.profileId),
                 header = mapOf<String, Any>(HEADER_TOKEN_TYPE to TokenType.ACCESS.toString()),
                 expiresOn = Date(System.currentTimeMillis() + 1000 * jwtProperties.accessLifeTime)
         )
@@ -36,8 +36,14 @@ class TokenService(private val jwtProperties: JWTProperties,
         return UUID.fromString(decodedJWT.getClaim(PERSON_ID_CLAIM).asString())
     }
 
+    fun getProfileId(token: String): UUID {
+        val decodedJWT = jwtService.decodeJWT(token)
+        return UUID.fromString(decodedJWT.getClaim(PROFILE_ID_CLAIM).asString())
+    }
+
     companion object {
         private const val HEADER_TOKEN_TYPE = "type"
         private const val PERSON_ID_CLAIM = "personID"
+        private const val PROFILE_ID_CLAIM = "profileID"
     }
 }
